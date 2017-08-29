2bc1095c0055ab2607fae67f66584a42d0aa1599
David Pauli <bepeppered@gmail.com>
2017-08-30 00:18:05 +0200

    Enhancement/#168 add lots of tests (#174)
    * Add several tests
    
    * Move tests in correct folder.
    
    * Extends tests
    
    * Add 0.2.1

--- src/configuration/config.php
+++ src/configuration/config.php
@@ -4,6 +4,6 @@
  *
  * @ignore
  */
-$FRAMEWORK_VERSION		= "0.2.0";	// The version of this Framework.
+$FRAMEWORK_VERSION		= "0.2.1";	// The version of this Framework.
 $BUILD_SCRIPT_VERSION	= "0.0.2";	// The version of the build script.
 ?>
